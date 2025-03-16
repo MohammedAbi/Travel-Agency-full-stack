6725e9691697a14e738338512f2a0f5428595d37
Mohammed Abdul Abi <mohammedabdulabi@gmail.com>
2025-03-16 03:19:48 +0100

    linked dist folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Ensures assets are linked correctly when deployed
+  build: {
+    outDir: "dist", // Ensure the build is outputted to 'dist' folder
+  },
+});
